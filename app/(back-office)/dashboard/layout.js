b273cb7eafce14d933953721689f022dafad60c1
roy <wlgnsdl7368@gmail.com>
2024-08-26 17:02:40 +0900

    사이드바 작업

--- app/(back-office)/dashboard/layout.js
+++ app/(back-office)/dashboard/layout.js
@@ -1,15 +1,17 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Sidebar from "@/components/backoffice/Sidebar";
 import Navbar from "@/components/backoffice/Navbar";
 
 export default function layout({ children }) {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div className="flex">
       {/* 사이드바 */}
-      <Sidebar />
+      <Sidebar showSidebar={showSidebar} />
       <div className="flex-grow w-full min-h-screen ml-0 bg-slate-100">
         {/* 헤더 */}
-        <Navbar />
+        <Navbar setShowSidebar={setShowSidebar} />
         <main className="p-8 mt-16 dark:bg-slate-900 text-slate-50">
           {children}
         </main>
