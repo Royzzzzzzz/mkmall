1e8ed8136be34f9a83a5c2280465a9a1a9fe9c5a
roy <wlgnsdl7368@gmail.com>
2024-09-03 17:16:46 +0900

    전송 버튼까지 작업

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -1,29 +1,48 @@
 "use client";
 import FormHeader from "@/components/backoffice/FormHeader";
+import SubmitButton from "@/components/FormInputs/SubmitButton";
 import TextInput from "@/components/FormInputs/TextInput";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function NewCategory() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
-    return (
-        <div>
-            <FormHeader title="새로운 카테고리 추가" />
-            <form className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"></form>
-            <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                <TextInput label="Category Title" name="title" register={register} errors={errors} />
-            </div>
-            {/* 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  async function onSubmit(data) {
+    console.log(data);
+  }
+  return (
+    <div>
+      <FormHeader title="새로운 카테고리 추가" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-4xl p-4 mx-auto my-3 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+          <TextInput
+            label="Category Title"
+            name="title"
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <SubmitButton
+          isLoading={false}
+          buttonTitle="Create Category"
+          loadingButtonTitle="Create Category please wait..."
+        />
+      </form>
+
+      {/* 
       -id
       -title
       -slug
       -description
       -image
        */}
-        </div>
-    );
+    </div>
+  );
 }
