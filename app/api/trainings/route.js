d010348eab54a259bba902a07d7b6c8acc10957e
roy <wlgnsdl7368@gmail.com>
2024-10-15 21:27:14 +0900

    커뮤니티 생성 및 불러오기까지완료

--- app/api/trainings/route.js
+++ app/api/trainings/route.js
@@ -1,3 +1,4 @@
+import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
@@ -11,15 +12,32 @@ export async function POST(request) {
       isActive,
       content,
     } = await request.json();
-    const newTraining = {
-      title,
-      slug,
-      imageUrl,
-      categoryId,
-      description,
-      isActive,
-      content,
-    };
+    //Check if this training already exists
+    const existingTraining = await db.training.findUnique({
+      where: {
+        slug,
+      },
+    });
+    if (existingTraining) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "이미 같은교육이 존재합니다",
+        },
+        { status: 409 }
+      );
+    }
+    const newTraining = await db.training.create({
+      data: {
+        title,
+        slug,
+        categoryId,
+        imageUrl,
+        description,
+        isActive,
+        content,
+      },
+    });
     console.log(newTraining);
     return NextResponse.json(newTraining);
   } catch (error) {
@@ -27,6 +45,26 @@ export async function POST(request) {
     return NextResponse.json(
       {
         message: "트레이닝 생성 실패",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(request) {
+  try {
+    const trainings = await db.training.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return NextResponse.json(trainings);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "트레이닝 가져오기 실패",
         error,
       },
       { status: 500 }
