fcdd1400130bd9c5d26de170c697dd0757496b28
roy <wlgnsdl7368@gmail.com>
2024-10-16 18:28:14 +0900

    뉴

---
+++ components/frontend/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchForm from "./SearchForm";
+
+export default function Navbar() {
+  return (
+    <div>
+      <a href="#">로고</a>
+      <SearchForm/>
+    </div>
+  );
+}
