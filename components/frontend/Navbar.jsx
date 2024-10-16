681164baef600a09311fc009bcd624769cc1849e
송지훈 <wlgnsdl7368@gmail.com>
2024-10-17 00:13:09 +0900

    네비바 작업중

--- components/frontend/Navbar.jsx
+++ components/frontend/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import SearchForm from "./SearchForm";
-
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/mkmall_logo.png";
 export default function Navbar() {
-  return (
-    <div>
-      <a href="#">로고</a>
-      <SearchForm/>
-    </div>
-  );
+    return (
+        <div className="bg-gray-50">
+            <div className="flex items-center justify-between py-3 max-w-7xl mx-auto px-8 gap-8">
+                {/* Logo */}
+                <Link className="" href="/">
+                    <Image src={logo} alt="limifood logo" className="w-24" />
+                </Link>
+                {/* SEARCH */}
+                <div className="flex-grow">
+                    <SearchForm />
+                </div>
+            </div>
+        </div>
+    );
 }
