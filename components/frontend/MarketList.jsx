c0ee212cfebef09e8584f019c73065b96936d5a2
roy <wlgnsdl7368@gmail.com>
2024-11-11 18:21:34 +0900

    다중 캐러셀 작업중

--- components/frontend/MarketList.jsx
+++ components/frontend/MarketList.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import MarketsCarousel from "./MarketsCarousel";
 
 export default function MarketList() {
-    return (
-        <div>
-            <h2>마켓 리스트입니다</h2>
-            {/* 마켓 슬라이더 */}
-            <MarketsCarousel />
-        </div>
-    );
+  return (
+    <div className="py-16 text-white">
+      <h2>마켓 리스트입니다</h2>
+      {/* 마켓 슬라이더 */}
+      <MarketsCarousel />
+    </div>
+  );
 }
